a31e688da0c1d883cba87a6947af49f4a2a69630
BenWix <benwichser@gmail.com>
2020-05-29 19:52:51 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-  def user_attributes=(user)
-    binding.pry
-  end
-
-  accepts_nested_attributes_for :user
+  accepts_nested_attributes_for :user, reject_if: proc {|att| att['username'].empty?}
 end
