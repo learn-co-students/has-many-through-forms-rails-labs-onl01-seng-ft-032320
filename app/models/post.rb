37b5176965dfc41a113cbaba331763a8ee2fa3de
Justin Winger <justinwinger4@gmail.com>
2020-06-04 14:58:37 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,19 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(categories_attributes)
+    categories_attributes.values.each do |cat|
+      if cat[:name].present?
+        category = Category.find_or_create_by(cat)
+        if !self.categories.include?(category)
+          self.post_categories.build(category: category)
+        end
+      end
+    end
+  end
+
+  def fields_blank?
+    self.content.epty? || self.user.empty?
+  end
 
 end
