656484dca02f5ec7493a56480293de354188921d
antdp425 <antdp425@aol.com>
2020-06-05 00:21:09 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,14 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  # accepts_nested_attributes_for :user
 
+  def user_attributes=(user_atts)
+    user_atts.values.each do |u|
+      if u.present?
+        user = User.find_or_create_by(username: u)
+        self.user = user
+      end
+    end
+  end
 end
