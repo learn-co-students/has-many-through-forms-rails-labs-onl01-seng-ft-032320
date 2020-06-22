2a1205381e92625493f8bb1451abee092ac07b22
nisticism <nistacular@gmail.com>
2020-06-22 13:10:34 -0700

    Done.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,14 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    
+    @posts = []
+
+    PostCategory.all.each do |pc|
+      if pc.category_id == @category.id
+        @posts << Post.find_by(id: pc.post_id)
+      end
+    end
+
   end
 end
